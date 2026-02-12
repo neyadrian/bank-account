66bb5ba7ce22a6c5580b7c9c3a2509c720008ab8
neyadrian <neyadrian2018@gmail.com>
2026-02-12 20:16:44 -0300

    criando o construtor e primeiros métodos

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,15 @@
 public class Account {
+
+    public int accountNumber;
+    public String holderName;
+    public double currentBalance = 0.0;
+
+    public Account(int accountNumber, String holderName) {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+    }
+
+    public double deposit() {
+
+    }
 }
