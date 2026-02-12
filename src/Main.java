ca775637471be8a60bd00ab01b822497317b5ccd
neyadrian <neyadrian2018@gmail.com>
2026-02-12 20:34:32 -0300

    iniciando o main

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,18 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter account number: ");
+        int accountNumber = sc.nextInt();
+        System.out.print("Enter account holder: ");
+        String holderName = sc.nextLine();
+        System.out.print("Is there na inital deposit (y/n)? ");
+        char op = sc.next().charAt(0);
 
+        Account account = new Account(accountNumber, holderName);
+
+        if (op == 'y') {
+            System.out.println("Enter initial deposit value: ");
+        }
 
         sc.close();
     }
